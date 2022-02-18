dd1478e9517dfc8194828411adbc80302207effb
Jordan-Morris20 <jordanmorris.za@gmail.com>
2022-02-18 12:30:35 +0200

    Added explanatory comments

--- Easy/fashion-classification.py
+++ Easy/fashion-classification.py
@@ -3,16 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras import regularizers
 
+# Load and organise data 
 fashion_mnist = tf.keras.datasets.fashion_mnist
-
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() 
-
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+# Normalise for input 
 train_images = train_images / 255
 test_images = test_images / 255
 
+# Create model
 def get_model():
     model = tf.keras.Sequential([
         tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -27,9 +28,11 @@ def get_model():
     return model
 
 
-
+# Train
 model=get_model()
 history=model.fit(train_images, train_labels, epochs=20)
+
+# Compute and report accuracy
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose =2)
 print('\nTest accuracy:', test_acc)
 
--- Easy/imdb-classification.py
+++ Easy/imdb-classification.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from keras import regularizers
 from keras.datasets import imdb
 
+# Load and vectorise data
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
 def vectorize_sequences(sequences, dimension=10000):
@@ -14,11 +15,11 @@ def vectorize_sequences(sequences, dimension=10000):
 
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
-
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
 
 
+# Create model
 def get_model():
     model = tf.keras.Sequential([
             tf.keras.layers.Dense(16, activation='relu'),
@@ -31,20 +32,21 @@ def get_model():
             metrics=['accuracy'])    
     return model
 
+# Seperate data into train and validation sets
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
 
-
-
+# Train
 model = get_model()       
 history=model.fit(partial_x_train, partial_y_train, epochs=13)
 
+# Compute and report accuracy
 test_loss, test_acc =model.evaluate(x_val, y_val, verbose=2)
-print('\nTest Accuracy:', round(test_acc, 3))
+print('\nTest Accuracy:', test_acc)
 
-
+# Plotting 
 loss=history.history['loss']
 accuracy=history.history['accuracy']
 plt.plot(loss, label='loss')
